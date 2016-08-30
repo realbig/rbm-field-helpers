517668e5e6c2192ca432db75399b234da5bf8e4f
joelworsham <joel.worsham@gmail.com>
2016-08-30 14:33:41 -0400

    Fix some repeater bugs

--- assets/js/admin/repeater.js
+++ assets/js/admin/repeater.js
@@ -16,21 +16,19 @@
 
                     $(this).trigger('add-item', [$(this)]);
                 },
-                hide: function () {
+                hide: function (deleteElement) {
 
                     if (confirm('Are you sure you want to delete this element?')) {
-                        $(this).slideUp( {
-                            duration: 400,
-                            complete: function () {
-                                // We can't call events on the removed item, so we will call it on the main Repeater
-                                var $repeater = $(this).closest('.rbm-field-repeater');
-                                $(this).remove();
-                                $repeater.trigger('delete-item', [$repeater]);
-                            },
-                        } );
+
+                        $(this).slideUp(400, deleteElement);
+                        $(this).trigger('delete-item', [$(this)]);
                     }
                 },
-                isFirstItemUndeletable: true
+                ready: function (setIndexes) {
+
+                    $list.on('sortupdate', setIndexes);
+                },
+                isFirstItemUndeletable: false
             });
 
             $list.sortable({
@@ -39,20 +37,6 @@
                 forcePlaceholderSize: true,
                 placeholder: 'rbm-sortable-placeholder',
                 update: function (e, ui) {
-
-                    // Make sure the int in each attr name is the same as the row index
-                    $list.find('.rbm-field-repeater-row').each(function () {
-
-                        $(this).find('input, select, textarea').filter('[name^="' + name + '"]').each(function () {
-
-                            var index = $(this).closest('.rbm-field-repeater-row').index(),
-                                attr_name = $(this).attr('name'),
-                                re = new RegExp('(' + name + '\\[)\\d(\\]\\[.*?\\])', 'i');
-
-                            $(this).attr('name', attr_name.replace(re, "$1" + index + "$2"));
-                        });
-                    });
-
                     $list.trigger('list-update', [$(this).closest('.rbm-field-repeater')]);
                 }
             });
