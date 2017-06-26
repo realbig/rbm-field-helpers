b8e75967c2037d7d73560d04d036ada5502fc48a
joelworsham <joel.worsham@gmail.com>
2017-06-26 14:39:01 -0400

    Datepicker repeater fix

--- assets/js/admin/datepicker.js
+++ assets/js/admin/datepicker.js
@@ -6,15 +6,29 @@
         var $datepickers = $('.rbm-field-datepicker'),
             $repeaters = $('.rbm-field-repeater');
 
+        if ($repeaters.length) {
+
+            $repeaters.on('add-item', datepicker_repeater_reinitialize);
+
+            // Reinitializes on init so that datepicker initializes with proper name attribute that the repeater sets.
+            $repeaters.on('repeater-init', datepicker_repeater_reinitialize);
+        }
+
         if ($datepickers.length) {
+
             datepickers_init($datepickers);
         }
 
-        if ($repeaters.length) {
-            $repeaters.on('add-item', datepicker_repeater_add);
-        }
-
-        function datepicker_repeater_add(e, $item) {
+        /**
+         * Takes the datepickers, "un-initializes" them, then initializes
+         * them again.
+         *
+         * @since {{VERSION}}
+         *
+         * @param e
+         * @param $item
+         */
+        function datepicker_repeater_reinitialize(e, $item) {
 
             var $datepickers = $('.rbm-field-datepicker');
 
@@ -23,6 +37,13 @@
             datepickers_init($datepickers);
         }
 
+        /**
+         * Initializes datepickers.
+         *
+         * @since {{VERSION}}
+         *
+         * @param $datepickers
+         */
         function datepickers_init($datepickers) {
 
             $datepickers.each(function (){
@@ -42,6 +63,7 @@
                     }
                 });
 
+                console.log('[name="' + name + '"]');
                 options['altField'] = '[name="' + name + '"]';
                 options['altFormat'] = 'yymmdd';
 
