0c5e67f04f70ca5defdcd8e4ae0dd81d6038874f
Eric Defore <eric.defore@gmail.com>
2017-06-29 10:29:14 -0400

    v1.3.7

--- rbm-field-helpers.php
+++ rbm-field-helpers.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: RBM Field Helpers
  * Description: Provides helper functions shared among all RBM plugins.
- * Version: 1.3.6
+ * Version: 1.3.7
  * Author: Real Big Marketing
  * Author URI: http://realbigmarketing.com
  * GitHub Plugin URI: realbig/rbm-field-helpers
@@ -25,7 +25,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( ! defined( 'RBM_HELPER_FUNCTIONS' ) ) {
 
 	define( 'RBM_HELPER_FUNCTIONS', true );
-	define( 'RBM_FIELD_HELPERS_VER', '1.3.6' );
+	define( 'RBM_FIELD_HELPERS_VER', '1.3.7' );
 
 	final class RBM_FieldHelpers {
 
