64f78cce7acdd9e5979d3b06b5e8422da3811a39
joelworsham <joel.worsham@gmail.com>
2017-11-01 18:57:45 -0400

    Toggle and styles

---
+++ core/fields/views/fields/field-toggle.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Field Template: Checkbox
+ *
+ * @since {{VERSION}}
+ *
+ * @var array $args Field arguments.
+ * @var string $name Field name.
+ * @var mixed $value Field value.
+ */
+
+defined( 'ABSPATH' ) || die();
+?>
+
+<div class="fieldhelpers-field-checkbox-container"
+     data-fieldhelpers-field-checkbox
+>
+	<?php $i = 0; ?>
+	<?php foreach ( $args['options'] as $check_value => $check_label ) : ?>
+		<?php $i ++; ?>
+
+        <div class="fieldhelpers-field-checkbox-row">
+
+            <input type="checkbox"
+                   name="<?php echo $name . ($args['repeater'] ? '' : '[]'); ?>"
+                   id="<?php echo "{$args['id']}_{$i}"; ?>"
+                   value="<?php echo $check_value; ?>"
+                   class="<?php echo $args['input_class']; ?>"
+				<?php echo in_array( $check_value, $value ) ? 'checked' : ''; ?>
+				<?php RBM_FH_Field::input_atts( $args ); ?>
+            />
+
+            <label for="<?php echo "{$args['id']}_{$i}"; ?>"
+                   class="fieldhelpers-field-checkbox-label"
+            ><?php echo $check_label; ?></label>
+
+        </div>
+
+	<?php endforeach; ?>
+</div>
